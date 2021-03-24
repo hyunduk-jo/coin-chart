a7aaba9664c16769bec8838e23a29a5c4a076743
hyunduk-jo <johd321@kakao.com>
2021-03-24 13:28:44 +0900

    fix display crashing

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -1,18 +1,18 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../scss/Components/Coin.scss';
 
-const Coin = ({coins}) => {
+const Coin = ({ coins }) => {
   //console.log(coins)
   return <div className="wrapper">
     {coins.map((coin, i) => <div className={`coin-container ${(i + 2) % 2 === 0 ? "even" : "odd"}`} key={coin.name}>
-      <Link to={`/coin/${coin.id}`}><img src={coin.image} alt={coin.id} /></Link>
-      <Link to={`/coin/${coin.id}`}><span className="coin-id">{coin.id}</span></Link>
-      <Link to={`/coin/${coin.id}`}>
+      <Link to={`/coin/${coin.id}`} className="s"><img src={coin.image} alt={coin.id} /></Link>
+      <Link to={`/coin/${coin.id}`} className="s"><span className="coin-id">{coin.id}</span></Link>
+      <Link to={`/coin/${coin.id}`} className="s">
         <span className="coin-symbol">{coin.symbol.toUpperCase()}</span>
       </Link>
-        <span>{coin.current_price.toLocaleString()} KRW</span>
-        <span className={coin.price_change_percentage_24h === null ? 'black' : coin.price_change_percentage_24h > 0 ? 'green' : 'red'}>{coin?.price_change_percentage_24h ? coin.price_change_percentage_24h.toFixed(2) : 0}%</span>
-        <span>Mkt Cap: ₩{coin.market_cap.toLocaleString()}</span>
+      <span>{coin.current_price.toLocaleString()} KRW</span>
+      <span className={coin.price_change_percentage_24h === null ? 'black' : coin.price_change_percentage_24h > 0 ? 'green' : 'red'}>{coin?.price_change_percentage_24h ? coin.price_change_percentage_24h.toFixed(2) : 0}%</span>
+      <span className="mkt-cap">Mkt Cap:<br />₩{coin.market_cap.toLocaleString()}</span>
     </div>
     )}
   </div>
